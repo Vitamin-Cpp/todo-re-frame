d64d9bd4661a11bf7937f699bea793a4d11c260c
Thapelo Mafiri <thapelom@navigare.co.za>
2023-07-19 09:41:26 +0200

    chore: setup db with dummy data for building ui

--- src/todo_re_frame/db.cljs
+++ src/todo_re_frame/db.cljs
@@ -1,4 +1,16 @@
 (ns todo-re-frame.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:todos {0 {:id 0
+              :description "Register so effect and cofx"
+              :status :active}
+           3 {:id 3
+              :description "Make sure to use interceptors somehow"
+              :status :active}
+           1 {:id 1
+              :description "Subscribe to db event"
+              :status :active}
+           2 {:id 2
+              :description "Emit db event for todo app"
+              :status :active}}
+   :showing :all})
