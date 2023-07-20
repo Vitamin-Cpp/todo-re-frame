8d6567266a6fd0ed70d67506ab8d4b5c75b5fa7a
Thapelo Mafiri <thapelom@navigare.co.za>
2023-07-20 12:05:24 +0200

    chore: remove dummy data used for development

--- src/todo_re_frame/db.cljs
+++ src/todo_re_frame/db.cljs
@@ -1,18 +1,7 @@
 (ns todo-re-frame.db)
 
 (def default-db
-  {:todos {0 {:id 0
-              :description "Register so effect and cofx"
-              :status :active}
-           3 {:id 3
-              :description "Make sure to use interceptors somehow"
-              :status :active}
-           1 {:id 1
-              :description "Subscribe to db event"
-              :status :active}
-           2 {:id 2
-              :description "Emit db event for todo app"
-              :status :active}}
+  {:todos {}
    :next-todo-id 0
    :next-todo-description ""
    :showing :all})
